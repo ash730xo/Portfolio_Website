c3f2660a54c3dd5f1375d8808d57610b8a9d8eaa
ash730xo <ash730@gmail.com>
2023-09-28 10:45:51 -0400

    restarting calculator project confused self

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 import Login_page from '../Components/Login_page'
-import Calculator from '../Components/Calculator'
+
 
 function Projects() {
   return (
     <div>
       <h1> Project 1 </h1>
-      {/* <Login_page /> */}
-      <Calculator />
+      <Login_page />
+
     </div>
   )
 }
