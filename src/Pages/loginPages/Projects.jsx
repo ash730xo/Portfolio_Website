828179d2a39692d313e71caf2f808d34a9d0e946
ash730xo <ash730@gmail.com>
2023-10-04 12:46:42 -0400

    home page update

--- src/Pages/loginPages/Projects.jsx
+++ src/Pages/loginPages/Projects.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-// import Login_page from '../../Components/Login_page'
+import Login_page from '../../Components/Login_page'
 import Calculator from '../../Components/Calculator'
 
 function Projects() {
   return (
     <div>
       <h1> Project 1 </h1>
-      {/* <Login_page /> */}
+      <Login_page />
       <Calculator />
 
     </div>
