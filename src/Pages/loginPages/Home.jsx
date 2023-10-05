520fa2a203bfebd6f40ef1043d68a104ccae00f7
ash730xo <ash730@gmail.com>
2023-10-05 12:21:23 -0400

    trying to make background photo for home page

--- src/Pages/loginPages/Home.jsx
+++ src/Pages/loginPages/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import '../../Styles/Home.css'
+
+import home_Photo from './../../Images/home.jpg'
+
 function Home() {
   return (
-    <div className='div_container'>
-      Home
+    <div className='home'>
+      <img src={home_Photo} alt="home_photo"/>
     </div>
   )
 }
